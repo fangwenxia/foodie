6d85e9cc1fab676aecf4b841d0dcc12a5447e782
Leah Teffera <lteffera@wellesley.edu>
2021-03-18 23:28:34 -0400

    Trying to move all my progress to the main branch. We'll see how this goes.

--- draft/load-food.sql
+++ draft/load-food.sql
@@ -4,8 +4,3 @@ fields terminated by ','
 lines terminated by '\n'
 ignore 1 lines;
 
--- load data local infile "comments.csv"
--- into table feedback
--- fields terminated by ','
--- lines terminated by '\n'
--- ignore 1 lines; 
