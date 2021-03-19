93dda335965552cfcee4098e87dcebb8d3dc5cd4
Leah Teffera <lteffera@wellesley.edu>
2021-03-18 23:24:12 -0400

    Updated comments data to successfully add the comments from the csv file to the database.

--- draft/load-comments.sql
+++ draft/load-comments.sql
@@ -1,3 +1,6 @@
+insert into student(username, name, favoriteDH, favoriteFood)
+    values ('lteffera','Leah Teffera',1, 1001) --ONLY RUN ONCE!! 
+
 load data local infile "comments_data_updated.csv"
 into table feedback
 fields terminated by ','
