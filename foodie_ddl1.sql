0d5685b80438141b42045374c9d5cf1d4e08b29f
Fangwen Xia <fangwenxia>
2021-03-19 06:02:11 -0400

    added column in Feedback table in ddl

--- foodie_ddl1.sql
+++ foodie_ddl1.sql
@@ -1,13 +1,16 @@
--- Sara Clark, Fangwen Xia, Leah Teffera, Gigi Gabeau
+ -- Sara Clark, Fangwen Xia, Leah Teffera, Gigi Gabeau
+
 drop table if exists labels;
 drop table if exists feedback;
 drop table if exists student;
 drop table if exists food;
-drop table if exists DHstaff;
 drop table if exists diningHall;
 
 use foodie_db;
-
+-- CHANGES:
+-- need to be able to search menu by current location of a food item, potential solution: 
+-- have variable for last served time and location
+-- adding datetime in feedback table
 create table diningHall(
      did int not null, -- dining hall id
      name varchar(30),
@@ -16,16 +19,6 @@ create table diningHall(
      primary key (did)
     )
     ENGINE = InnoDB;
-
-create table DHstaff ( -- dining hall staff
-    sid int not null, -- staff id
-    name varchar(30),
-    worksAt int not null, -- id of dining hall
-    primary key (sid),
-    foreign key (worksAt) references diningHall(did)
-        on update restrict
-        on delete restrict)
-ENGINE = InnoDB;
 
 create table food(
      fid int not null, -- food id
@@ -60,7 +53,7 @@ create table feedback(
      rating int,-- rating from 1 to 5, 5 being would recommend and 1 being would not recommend
      username varchar(15), -- user giving feedback
      fid int, -- id of food being critiqued
-     index(comment),
+     entered datetime,
      primary key (username, fid),
      foreign key (username) references student(username)
         on update restrict
@@ -74,8 +67,8 @@ create table feedback(
 create table labels(
     lid int not null, -- label id
     -- from wellesley fresh website
-    allergen set('eggs', 'fish', 'milk', 'peanuts', 'shellfish', 'soy', 'tree nut', 'wheat'),
-    preference set('gluten sensitive', 'vegan', 'vegetarian', 'kosher', 'halal'),
+    allergen set('no allergens','eggs', 'fish', 'milk', 'peanuts', 'shellfish', 'soy', 'tree nut', 'wheat'),
+    preference set('none','gluten sensitive', 'vegan', 'vegetarian', 'kosher', 'halal'),
     ingredients varchar(80),
     fid int,
     primary key (lid),
@@ -96,26 +89,31 @@ create table labels(
     insert into diningHall (did, name, hours, waitTime)
     values (5, 'Tower', '7:30am to 7:30pm', 15);
 
-    insert into DHstaff (sid, name, worksAt)
-    values (1, 'Tim', 1);
-
     insert into food (fid, name, lastServed, type, did)
     values (1, 'Taco Bowl', '2021-03-02', 'lunch', '2');
 
     insert into food (fid, name, lastServed, type, did)
     values (2, 'Egg Sandwich', '2021-03-02', 'breakfast', '5');
 
-    insert into labels (lid, allergen, ingredients, fid)
-    values (1, 'eggs,milk,wheat', 'english muffin, fried egg', 2);
+    insert into food (fid, name, lastServed, type, did)
+    values (3, 'Beef Stroganoff', '2020-04-18', 'lunch', '5');
+    insert into labels (lid, allergen, preference, ingredients, fid)
+    values (3, 'wheat, milk', 'none', 'egg noodles', 3);
 
-    insert into labels (lid, preference, ingredients, fid)
-    values (2, 'vegan', 'avocado', 1);
+    insert into labels (lid, allergen, preference, ingredients, fid)
+    values (1, 'eggs,milk,wheat', 'english muffin', 'none', 2);
+
+    insert into labels (lid, allergen, preference, ingredients, fid)
+    values (2, 'no allergens', 'vegan', 'avocado', 1);
 
     insert into student(username, name, favoriteDH, favoriteFood)
     values ('sclark4', 'Sara Clark', 2, 1);
 
-    insert into feedback(comment, rating, username, fid)
-    values ('Taco Tuesday at Lulu was great! The cilantro crema was weirdly sweet though.', 4, 'sclark4', 1);
+    insert into student(username, name, favoriteDH, favoriteFood)
+    values ('fx1', 'Fangwen Xia', 5, 3);
 
-    insert into feedback(comment, rating, username, fid)
-    values ('The croissant sandwich is ok, but I wish the fried eggs were not cooked so hard', 3, 'sclark4', 2);
+    insert into feedback(comment, rating, username, fid,entered)
+    values ('Taco Tuesday at Lulu was great! The cilantro crema was weirdly sweet though.', 4, 'sclark4', 1,now());
+
+    insert into feedback(comment, rating, username, fid,entered)
+    values ('The croissant sandwich is ok, but I wish the fried eggs were not cooked so hard', 3, 'sclark4', 2,now());
