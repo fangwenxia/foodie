d2a28e5f37819092a319557868854e14ab8d9309
Leah Teffera <lteffera@wellesley.edu>
2021-03-20 18:15:35 -0400

    Data entry form with full-functionality added. Changed endpoint from insert to addfood, updated html files accordingly.

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import cs304dbi as dbi
 import menuUpdates as menuUp #module to update foodie database from the menu page
 import random
 import sys
+import pymysql
 import feed_queries
 import query
-
 
 from datetime import date,datetime
 
@@ -273,25 +273,14 @@ def review(): #rename review() to feed
 
     # LEAH's STUFF
 
-def handleErrors(name,date,category,hall,preferences,allergens,ingredients): 
-    if food_name is None: 
-        message = "missing input: Food name is missing."
-    elif food_category is None: 
-        message = "missing input: Food category is missing."
-    elif food_dhall is None:
-        message = "missing input: Food dining hall is missing."
-    elif food_preferences is None:
-        message = "missing input: Food preferences is missing."
-    elif food_allergens is None: 
-        message = "missing input: Food allergens is missing."
-    elif food_ingredients is None: 
-        message = "missing ingredients: Food ingredients are missing."
+def handleErrors(name,category,hall,preferences,allergens,ingredients): 
+    message="hello"
     return message
 
 @app.route('/addfood/', methods=["GET", "POST"])
-def insert():
+def addfood():
     if request.method == 'GET':
-        return render_template('dataentry.html', action=url_for('insert'))
+        return render_template('dataentry.html', action=url_for('addfood'))
     elif request.method == 'POST':
         food_name = request.form.get('food-name') 
         food_category = request.form.get('food-type')
@@ -299,25 +288,58 @@ def insert():
         food_preferences = request.form.get('food-preferences')
         food_allergens = request.form.get('food-allergens')
         food_ingredients = request.form.get('food-ingredients')
+        print([food_name,food_category,food_dhall,food_preferences,food_allergens,food_ingredients])
         error_messages = []
-        message = handleErrors(food_name,food_category,food_dhall,food_preferences,food_allergens,food_ingredients)
-        error_messages.append(message)
-        if len(error_messages) > 0:
-            return render_template('dataentry.html', action=url_for('insert'))
-        flash('form submission successful')
+        #message = handleErrors(food_name,food_category,food_dhall,food_preferences,food_allergens,food_ingredients)
+        # message = ""
+        # if food_name is None: 
+        #     message = "missing input: Food name is missing."
+        # elif food_category is None: 
+        #     message = "missing input: Food category is missing."
+        # elif food_dhall is None:
+        #     message = "missing input: Food dining hall is missing."
+        # elif food_preferences is None:
+        #     message = "missing input: Food preferences is missing."
+        # elif food_allergens is None: 
+        #     message = "missing input: Food allergens is missing."
+        # elif food_ingredients is None: 
+        #     message = "missing ingredients: Food ingredients are missing."
+        # error_messages.append(message)
+        # if len(error_messages) > 0:
+        #     return render_template('dataentry.html', action=url_for('addfood'), messages=error_messages)
+        # print("form submission successful.")
 
         #insert stuff into database
         connect = dbi.connect()
+        print("connected!")
         curs = dbi.cursor(connect)
-        sql = '''insert food(name,lastServed,type,did) 
-                  values (%s,%s,%s,%s,%s);'''
-        food_date = servertime.now()
+        sql = '''insert into food(name,lastServed,type,did) 
+                  values (%s,%s,%s,%s);'''
+        food_date = "2021-03-19"
         vals = [food_name,food_date,food_category,food_dhall]
         curs.execute(sql,vals)
         connect.commit()
+        print("success!")
         success_message = "Food {fname} inserted".format(fname=food_name)
         print(success_message)
-        return redirect(url_for('insert',messages=success_message))
+
+        curs1 = dbi.cursor(connect)
+        sql = '''select fid from food where name=%s'''
+        curs1.execute(sql,food_name)
+        food_id = curs1.fetchone()[0]
+        print(food_id)
+
+        curs2 = dbi.cursor(connect)
+        sql2 = '''insert into labels(allergen,preference,ingredients,fid) 
+                  values (%s,%s,%s,%s);'''
+        labelvals = [food_allergens,food_preferences,food_ingredients,food_id]
+        print(labelvals)
+        curs2.execute(sql2,labelvals)
+        connect.commit()
+
+        print("label inserted")
+
+        return redirect(url_for('addfood',messages=success_message,action='addfood'))
     
 @app.before_first_request
 def init_db():
@@ -331,7 +353,7 @@ if __name__ == '__main__':
         port = int(sys.argv[1])
         assert(port>1024)
     else:
-        # port = os.getuid()
-        port = 1200
+        port = os.getuid()
+        # port = 7739
     app.debug = True
     app.run('0.0.0.0',port)
