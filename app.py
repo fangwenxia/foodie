81dcaf9b8e595dd8200a3ce06ac42f1ee6704743
Sara Clark <sclark4@wellesley.edu>
2021-03-31 17:28:07 -0400

    minor bugs with css related to stars

--- app.py
+++ app.py
@@ -138,7 +138,7 @@ def food(fid):
         # list of dictionaries for each comment for a given food item and with the comment's rating and user
         comments = menuUp.lookupComments(conn, fid)
         filename = pic(fid)
-        return render_template('food.html', food = item, comments = comments, fid = fid, rating = int(avgRating), title = item["name"], filename = filename)
+        return render_template('food.html', food = item, comments = comments, fid = fid, rating = avgRating, title = item["name"], filename = filename)
     else:
         menuUp.updateLastServed(conn, fid, today()[0])
         return redirect(url_for('food', fid = fid))
